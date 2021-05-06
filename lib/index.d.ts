09dbe504eb34476b629412b5b58d6c3912f4c914
Im-Beast <franik.mateusz@gmail.com>
2021-05-06 14:32:14 +0200

    init

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import { Crayon } from 'crayon.js/lib/types';
+import { ColorKeyword } from './types';
+declare const _default: Crayon<ColorKeyword>;
+export default _default;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import crayon, { addStyles } from 'crayon.js'
+import { Crayon } from 'crayon.js/lib/types'
+import { colorKeywords } from './styles'
+import { ColorKeyword } from './types'
+
+addStyles(colorKeywords)
+
+export default crayon as Crayon<ColorKeyword>
